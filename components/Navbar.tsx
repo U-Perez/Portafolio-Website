d4cf3bcdd3eeedf3f5efd5197fcacdb10f6688da
U-Perez <uriasq.perez@gmail.com>
2024-01-27 11:36:52 -0500

    Add navbar and navitems

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {NavItems} from '@/lib/data'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <header className='z-[999] relative'>
+      <div className='fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'></div>
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {NavItems.map((item) => (
+            <li key={item.path} className='flex h-3/4 items-center justify-center relative'>
+                <Link className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition' key={item.path} href={item.path}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav> 
+    </header>
+  )
+}
+
+export default Navbar
+
+ 
---
+++ lib/data.tsx
@@ -0,0 +1,24 @@
+import { items } from "@/lib/type";
+
+export const NavItems: items[] = [
+  {
+    name: "Home",
+    path: "home",
+  },
+  {
+    name: "About ",
+    path: "about",
+  },
+  {
+    name: "Projects",
+    path: "projects",
+  },
+  {
+    name: "Skills",
+    path: "skills",
+  },
+  {
+    name: "Experience",
+    path: "experience",
+  },
+];
