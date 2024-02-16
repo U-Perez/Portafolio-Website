d7091669ec05d4dd5eea281780d068fd57e4102e
U-Perez <uriasq.perez@gmail.com>
2024-02-16 15:29:20 -0500

    Add projectspage & img

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutPage from "@/components/AboutPage";
 import HomePage from "@/components/HomePage";
+import ProjectsPage from "@/components/ProjectsPage";
 
 export default function Home() {
   return ( 
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HomePage/>
       <AboutPage/>
+      <ProjectsPage/>
     </main>
   );
 }
--- lib/data.tsx
+++ lib/data.tsx
@@ -39,3 +39,22 @@ export const skills = [
   { skill: "MySql" },
   { skill: "NPM" },
 ];
+
+export const projects = [
+  {
+    name: "ChatRoom",
+    description:
+      "Una Aplicación Web de mensajería en tiempo real con websockets implementados, login, registro y listado de contactos",
+    image: "/img/ChatRoom.png",
+    github: "https://github.com/U-Perez/Frontend-Chat",
+    link: "https://thankfulthoughts.io/",
+  },
+  {
+    name: "Edumark RRHH",
+    description:
+      "Edumark es diseñada para llevar un control y gestión de los empleados dentro de la plataforma, cuenta con 3 perfiles: empleados, empresa, sindicato cada perfil contiene información diferente donde podran interactuar",
+    image: "/img/EdumarkRH.png",
+    github: "https://github.com/U-Perez/Frontend-RH-Edumark",
+    link: "https://platoio.com/register",
+  },
+]; 
